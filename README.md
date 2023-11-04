770bfa09e5928cdb05ab463fbc818e3ff704653c
Omena0 <Omena0MC@gmail.com>
2023-11-04 20:04:38 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,56 @@
+
+# MChatGPT
+
+ Minecraft ChatGPT Bot
+
+## Setup
+
+1. Create an OpenAI API token
+2. Install python from [python.org](https://python.org/downloads)
+3. Download / clone this repo
+4. Create a new file called config.py
+5. Write this into it (fill in the blanks):
+
+    ```python
+    ## Basic config
+    API_KEY  = '<API KEY HERE>'
+    LOG_PATH = '<LOG PATH HERE>'
+
+    ## GPT Config
+    PREFIX       = 'gpt ' # The prefix used for requests.
+    INTERVAL     = 0.5    # Delay between checking latest.log.
+    TOKENLIMIT   = 128    # Maximum tokens ChatGPT can generate.
+    BANNED_USERS = []     # Users that cannot send requests.
+    EXTRA_INFO   = ''     # Info added to ChatGPT's first system message.
+
+    ## Chat Config
+    # Set this to the characted that splits the username and message.
+    CHAT_SEPARATOR = ':'
+
+    # Strings to remove from messages (includes username)
+    REMOVE_STRINGS = ['[VIP] ','[CHEESE] ',' DEFAULT ']
+
+    # If theese r in a username it will get ignored
+    IGNORE_STRINGS = ['[MINEHUT]']
+
+    ### ADVANCED CONFIG ###
+
+    # Removes everything from the first appearance of <value>
+    # to the direction specified.
+    #Format: {"value":"<value>","split":<0 or 1>} (0 = left, 1 = right)
+    CHAT_SPLIT = []
+    ```
+
+6. Open minecraft
+7. Run main.py and focus your minecraft window. Make sure to now have chat opened or anything.
+
+## Capabilities
+
+- Read up to 200 previous chat messages
+- Split messages after they become too long
+- Multiple requests at once
+- Doesent allow the same person to send another request before the last one is finished
+
+## Chat parsing
+
+a
---
+++ main.py
@@ -0,0 +1,165 @@
+
+import openai as ai
+import time as t
+import pyautogui as p
+from threading import Thread
+from textwrap import wrap
+import config
+
+ai.api_key = config.API_KEY
+path = config.LOG_PATH
+
+activeusers = []
+
+def sendResponse(chatMessage:list):
+    """Generates & sends a ChatGPT response to minecraft chat
+
+    Args:
+        chatMessage (list): Prompt for ChatGPT, 0: Username, 1: Message
+    """
+    try:
+        activeusers.append(chatMessage[0])
+        send(f'[GPT] Generating response... [{chatMessage[0]}]')
+        msg = getResponse(f'{chatMessage[0]}: {chatMessage[1]}',chatMessage[0]).content
+        msg.replace('\n',' ').strip()
+        print(f'[GPT] [{chatMessage[0]}] {msg}')
+        send(f'[GPT] [{chatMessage[0]}] {msg}')
+        activeusers.remove(chatMessage[0])
+    except Exception as e:
+        print(f'[ERROR] {e}')
+        send(f'[GPT] [ERROR] {e}')
+
+def getResponse(msg:str,user:str) -> list:
+    """Get a response from ChatGPT
+
+    Args:
+        msg (str): Prompt for ChatGPT
+        user (str): Username
+
+    Returns:
+        list: Response
+    """
+    print({"role":"user","content":msg})
+    history.append({"role":"system","content":f"Previous 100 chat messages: {last_chat}"})
+    history.append({"role":"user","content":msg})
+    response:ai.ChatCompletion = ai.ChatCompletion.create(
+      model="gpt-3.5-turbo",
+      messages=history,
+      temperature=0.1,
+      max_tokens=config.TOKENLIMIT,
+      user=user
+    )
+    message = response.choices[0].message
+    history.append(message)
+    return message
+
+def send(msg:str,noWrap=False):
+    """Send a message ingame
+
+    Args:
+        msg (str): Message to send, will be split if too long.
+        noWrap (bool): Prevent splitting messages. Defaults to False
+    """
+    msg = wrap(msg,255)
+    for i,v in enumerate(msg):
+        if i > 0 and not noWrap:
+            v = '[GPT] ' + v
+        p.hotkey('enter')
+        p.typewrite(v.replace('\n',''))
+        p.hotkey('enter')
+        
+
+def filter(msg:str) -> list[str]:
+    """Filters log messages to chat messages. Configurable in config.py
+
+    Args:
+        msg (str): Log message to fileter
+
+    Returns:
+        list[str]: 0: Sender's username, 1: Sent message
+    """
+    # Remove non chat messages
+    chatMessage = new.replace(f'[{t.strftime('%H:%M:%S')}] [Render thread/INFO]: [CHAT] ','<|>')
+    if not chatMessage.startswith('<|>') or '[AD]' in chatMessage: return
+    chatMessage = chatMessage.replace('<|>','')
+    
+    # Add to last chat
+    last_chat.append(chatMessage)
+    if len(last_chat) > 100: last_chat.pop(0)
+    print(f'[CHAT] {chatMessage}')
+    
+    # REMOVE_STRINGS
+    for i in config.REMOVE_STRINGS:
+        chatMessage = chatMessage.replace(i,'')
+    
+    # Split messages
+    if not config.CHAT_SPLIT in chatMessage: return
+    chatMessage = chatMessage.split(config.CHAT_SPLIT+' ',1)
+    
+    # CHAT_SEPARATOR
+    for value,dir in config.CHAT_SEPARATOR:
+        try: chatMessage.split(value,1)[dir]
+        except: pass
+    
+    # IGNORE_STRINGS
+    for i in config.IGNORE_STRINGS:
+        if i in chatMessage[0]: return
+        
+    # Make sure that banned users or users that are already generating a response are ingnored
+    if chatMessage[1].startswith(config.PREFIX) and chatMessage[0] not in config.BANNED_USERS and chatMessage[0] not in activeusers:
+        chatMessage[1] = chatMessage[1].replace(config.PREFIX,'')
+        Thread(target=sendResponse,args=[chatMessage],daemon=True).start()
+
+
+
+print('Starting...')
+
+old = ''
+
+last_chat:list[str] = []
+
+history:list[dict] = [{
+    "role":"system",
+    "content":f"""
+        You are a helpful assistant chatbot.
+        Messages will be supplied to you in the format of \"<user>: <message>\".
+        Do not prefix your responses with anything.
+        Answer in a short and concise way. The answers are supposed to fit in chat messages.
+        {config.EXTRAINFO}
+        Your Token limit is {config.TOKENLIMIT}.
+    """.replace('        ',' ')
+    }]
+
+send(f'[GPT] Starting... {config.PREFIX=}, {config.INTERVAL=}, {config.TOKENLIMIT=}')
+
+with open(path) as file:
+    print(f'Loading chat messages from latest.log')
+    for i in file.readlines():
+        i = filter(i)
+        if i == None: continue
+        last_chat.append(i)
+
+while True: 
+    try:
+        # Get Log entries
+        with open(path) as file:
+            new = file.readlines()[-1]
+        if new == old: continue
+        old = new
+
+        # Filter em
+        chatMessage = filter(new)
+        if chatMessage == None: continue
+
+        sendResponse(chatMessage)
+        
+        t.sleep(config.INTERVAL)
+    except: pass
+
+
+
+
+
+
+
+
